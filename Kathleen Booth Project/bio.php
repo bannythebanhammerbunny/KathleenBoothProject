1307cf3401678cb96a37a075d0f4b9f64f2477f3
bannythebanhammerbunny <bannythebanhammerbunny@gmail.com>
2025-02-07 13:29:57 -0500

    end of class commit

---
+++ Kathleen Booth Project/bio.php
@@ -0,0 +1,10 @@
+<?php
+include("h_f_front/header.php");
+?>
+<h1>Pre-Career</h1>
+<p>Kathleen Britten was born in England, on 9 July 1922.<br>
+Her first school is was at St Paul's Convent in Sutton Coldfield, and she sought further education at Sutton Coldfield High School.<br>
+</p>
+    <?php
+include("h_f_front/footer.php");
+?>
--- Kathleen Booth Project/h_f_front/header.php
+++ Kathleen Booth Project/h_f_front/header.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" type = "text/css" href = "style.css">
-    <title>Document</title>
+    <title>Kathleen Booth Project</title>
 
 </head>
 <body>
 
     <header>
         <ul id = "header">
-        <li><a href="index.php" style="float: left;"></a></li>
-        <li><a href="innovations.php"></a></li>
-        <li><a href = "bio.php">our products</a></li>
-        <li><a href = "diversity.php"></a></li>
-        <li><a href = "sources.php"></a></li>
+        <li><a href="index.php" style="float: left;">Home</a></li>
+        <li><a href="innovations.php">Contributions</a></li>
+        <li><a href = "bio.php">Biography</a></li>
+        <li><a href = "diversity.php">Why diversify?</a></li>
+        <li><a href = "sources.php">Sources</a></li>
         </ul>
     </header>
